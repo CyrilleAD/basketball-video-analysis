252fe353a12ebf510ff22e031c77d01fcc6fb636
Dady A. Cyrille <akroucyrille0501@gmail.com>
2025-07-25 22:58:47 -0400

    Add visualization modules (drawers) for basketball analysis
    Ajout des modules de visualisation:
    - drawers/: Modules pour dessiner les éléments d'analyse sur les frames
    - player_tracks_drawer.py: Visualisation des trajectoires des joueurs avec couleurs d'équipe
    - ball_tracks_drawer.py: Visualisation du suivi du ballon
    - court_key_points_drawer.py: Affichage des points clés du terrain
    - frame_number_drawer.py: Numérotation des frames
    - utils.py: Fonctions utilitaires pour dessiner ellipses et triangles
    
    Fonctionnalités de visualisation:
    - Couleurs distinctes par équipe
    - Indicateurs de possession du ballon
    - Annotations des points clés du terrain
    - Numérotation des frames pour le debug
    - Formes géométriques personnalisées
    
    Auteur: Dday Akrou Cyrille - Data Scientist
    Email: cyrilledady0501@gmail.com

---
+++ drawers/__init__.py
@@ -0,0 +1,12 @@
+# Basketball Video Analysis - Visualization Modules
+# Auteur: Dday Akrou Cyrille - Data Scientist
+# Email: cyrilledady0501@gmail.com
+
+from .player_tracks_drawer import PlayerTracksDrawer
+from .ball_tracks_drawer import BallTracksDrawer
+from .court_key_points_drawer import CourtKeypointDrawer
+from .team_ball_control_drawer import TeamBallControlDrawer
+from .frame_number_drawer import FrameNumberDrawer
+from .pass_and_interceptions_drawer import PassInterceptionDrawer
+from .tactical_view_drawer import TacticalViewDrawer
+from .speed_and_distance_drawer import SpeedAndDistanceDrawer
